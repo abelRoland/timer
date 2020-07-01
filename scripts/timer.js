05a316df88adf5b8df4c6ee491693b4fa967aaed
Mamé <mameazad.turgut@gmail.com>
2020-07-01 18:39:58 +0200

    finish timer with default time fixed #1

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,11 +1,12 @@
+//@ts-check;
 'use strict';
-
+//debugger;
 let default_minute = Number(document.getElementById('minute_timer').textContent);
 //console.log(default_minute, typeof default_minute);
 let count_second = Number(document.getElementById('second_timer').textContent);
 
 // get button label
-let button_label = document.getElementById('start').textContent;
+let button_label = document.getElementById('start-stop').textContent;
 
 //make global interval
 let set_interval;
@@ -13,22 +14,21 @@ let set_interval;
 // calculate total time
 
 function sumTime() {
-	return default_minute + count_second;
+	return default_minute * 60 + count_second;
 }
+let total_time = sumTime();
+//console.log(total_time);
 
 // change button status
-let button_status = document.getElementById('start');
+let button_status = document.getElementById('start-stop');
 
 function changeStatus(status) {
 	return status === 'Start' ? 'Stop' : 'Start';
 }
 
-document.getElementById('start').addEventListener('click', function () {
+document.getElementById('start-stop').addEventListener('click', function () {
 	//debugger;
 	button_status.innerHTML = changeStatus(button_label);
-
-	let total_time = sumTime();
-	console.log(total_time);
 
 	if (!set_interval) {
 		set_interval = setInterval(startTimer, 1000);
@@ -55,24 +55,22 @@ document.getElementById('start').addEventListener('click', function () {
 
 			--total_time;
 			if (total_time < 0) {
+				clearInterval(set_interval);
 				alert('time is up');
 				document.getElementById('minute_timer').innerHTML = 5;
 				document.getElementById('second_timer').innerHTML = '00';
-				document.getElementById('start').innerHTML = 'start';
+				button_status.innerHTML = changeStatus('Finish');
+				default_minute = Number(document.getElementById('minute_timer').textContent);
+				count_second = Number(document.getElementById('second_timer').textContent);
 				total_time = sumTime();
-				console.log(total_time);
-				console.log('minute', document.getElementById('minute_timer').textContent);
-				console.log(count_second);
-				clearInterval(set_interval);
 				set_interval = null;
 			}
 		}
 	} else {
+		//debugger;
 		clearInterval(set_interval);
 		set_interval = null;
-		document.getElementById('minute_timer').innerHTML = default_minute;
-		document.getElementById('second_timer').innerHTML = count_second;
-		document.getElementById('start').innerHTML = 'start';
+		button_status.innerHTML = changeStatus('Stop');
 	}
 
 	//
@@ -81,10 +79,13 @@ document.getElementById('start').addEventListener('click', function () {
 // reset
 
 document.getElementById('reset').addEventListener('click', function () {
+	//debugger;
+	clearInterval(set_interval);
 	document.getElementById('minute_timer').innerHTML = 5;
 	document.getElementById('second_timer').innerHTML = '00';
-	default_minute = 5;
-	button_label = 'start';
-	document.getElementById('start').innerHTML = button_label;
-	clearInterval(set_interval);
+	button_status.innerHTML = changeStatus('Stop');
+	default_minute = Number(document.getElementById('minute_timer').textContent);
+	count_second = Number(document.getElementById('second_timer').textContent);
+	total_time = sumTime();
+	set_interval = null;
 });
